1dde630a8a4d0c316e825af7ae9f8943ac9878f3
Zachary Guerrero <zack.grrr@gmail.com>
2022-09-27 13:12:30 -0700

    add roller

--- pages/api/roll/index.js
+++ pages/api/roll/index.js
@@ -1,7 +1,14 @@
 const { allowCors } = require("../util/")
+import { DiceRoller } from "@dice-roller/rpg-dice-roller"
 
 const handler = (req, res) => {
-  res.send({message: ''})
+  const rollString = req.query.ndn
+  const roller = new DiceRoller()
+  const str = decodeURI('3d6+4')
+  console.log(str)
+
+  const roll = roller.roll(rollString)
+  res.send(roll)
 }
 
 module.exports = allowCors(handler)
